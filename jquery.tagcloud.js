6e0a7481dcc0099db328c9e0e208282ad7ab837a
Tim Groeneveld <tim.g@webprofits.com.au>
2014-07-15 12:53:03 +1000

    Allow the class to be defined inside the options.

--- jquery.tagcloud.js
+++ jquery.tagcloud.js
@@ -6,7 +6,8 @@ jQuery.fn.tagCloud = function(cl, givenOptions) { //return this.each( function()
    var defaults = {
       sort: function (a, b) {return a.tag < b.tag ? -1 : (a.tag == b.tag ? 0 : 1)},//default sorting: abc
       click: function(tag) {},
-      maxFontSizeEm: 4,
+      maxFontSize: 4,
+      class: 'tagcloudlink',
       min: -1,
       max: -1
    }
@@ -16,9 +17,11 @@ jQuery.fn.tagCloud = function(cl, givenOptions) { //return this.each( function()
 
    // calculating the max and min count values,
    // but we only want to do it if we don't know the max & min values
+   var max = options.min,
+       min = options.max;
    if (max < 0 && min < 0) {
-       var max = -1;
-       var min = cl[0].count;
+       max = -1;
+       min = cl[0].count;
        $.each(cl, function(i, n) {
           max = Math.max(n.count, max);
           min = Math.min(n.count, min);
@@ -31,8 +34,8 @@ jQuery.fn.tagCloud = function(cl, givenOptions) { //return this.each( function()
 
    //Normalization helper
    var diff = ( max == min ? 1    // if all values are equal, do not divide by zero
-                           : (max - min) / (options.maxFontSizeEm - 1) ); //optimization: Originally we want to divide by diff
-                           // and multiple by maxFontSizeEm - 1 in getNormalizedSize.
+                           : (max - min) / (options.maxFontSize - 1) ); //optimization: Originally we want to divide by diff
+                           // and multiple by maxFontSize - 1 in getNormalizedSize.
    function getNormalizedSize(count) {
       return 1 + (count - min) / diff;
    }
@@ -41,7 +44,7 @@ jQuery.fn.tagCloud = function(cl, givenOptions) { //return this.each( function()
    this.empty();
    for (var i = 0; i < cl.length; ++i) {
       var tag = cl[i].tag;
-      var tagEl = jQuery('<a href="" class="tagcloudlink" style="font-size: '
+      var tagEl = jQuery('<a href="" class="' options.class + '" style="font-size: '
                            + getNormalizedSize(cl[i].count)
                            + 'em">' + tag + '<\/a>')
                   .data('tag', tag);
